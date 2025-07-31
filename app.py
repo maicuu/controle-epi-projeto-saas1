0f425265f21e27562fa64881a204730b35faa6c8
Maicon Wendel Cordeiro Costa <maiconwendel3010@gmail.com>
2025-07-31 13:30:04 -0300

    Finalizei a pagina inicial html+css, além de comentar

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)  # Isso deve vir antes de usar @app.route
+app = Flask(__name__)
 
 @app.route('/')
 def index():
---
+++ static/js/script.js
@@ -0,0 +1,2 @@
+// Mostra uma mensagem no console do navegador quando a página é carregada
+console.log("Script carregado com sucesso!");
